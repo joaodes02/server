7cc6a782c25cc1bb98c16adb1901617d50ddd07a
João Victor Silva <joaovictordes02@gmail.com>
2025-03-08 09:10:51 -0300

    model tracion

---
+++ prisma/migrations/20250308120826_model_tracion/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "Tracion" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "Tempera" TEXT NOT NULL,
+    "Item" TEXT NOT NULL,
+    "Bobina" TEXT NOT NULL,
+    "Espessura" REAL NOT NULL,
+    "LE" REAL,
+    "LR" REAL,
+    "Alogamento" REAL,
+    "R" REAL,
+    "N" REAL
+);
